6c8cf798ef2a9a9298594686ebc90ac3bffd0167
Mike Cotton <codingmusician@gmail.com>
2020-05-18 09:15:23 -0500

    Change input to select

--- src/admin-views/import-form/source.php
+++ src/admin-views/import-form/source.php
@@ -3,7 +3,7 @@
 		<label for="tribe-ea-field-<?php echo esc_attr( $origin ); ?>_import_source"><?php echo esc_html( $field->label ); ?></label>
 	</th>
 	<td>
-		<input
+		<select
 			type="hidden"
 			name="aggregator[<?php echo esc_attr( $origin ); ?>][source_type]"
 			id="tribe-ea-field-<?php echo esc_attr( $origin ); ?>_import_source"
@@ -12,7 +12,7 @@
 			data-prevent-clear
 			data-options="<?php echo esc_attr( json_encode( $field->options ) ); ?>"
 			value="<?php echo esc_attr( $default_eb_source ); ?>"
-		/>
+			></select>
 		<span class="tribe-bumpdown-trigger tribe-bumpdown-permanent tribe-bumpdown-nohover tribe-ea-help dashicons dashicons-editor-help" data-bumpdown="<?php echo esc_attr( $field->help ); ?>" data-width-rule="all-triggers"></span>
 	</td>
 </tr>
